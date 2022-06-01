ff32bbe48bf4d80eea954f0a30c9f074f4b283aa
arifian853 <arifiansaputra43@gmail.com>
2022-06-01 22:55:41 +0700

    Final commit + add something

---
+++ modal.js
@@ -0,0 +1,19 @@
+var modal = document.getElementById("MainModal");
+
+var btn = document.getElementById("ButtonModal");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
